5c47cf9faf1554659c261f280ce83c7f3b5d838f
AlexsandroD <adrdra88@gmail.com>
2021-12-09 15:33:25 +0100

    fine esercizio

---
+++ js/main.js
@@ -0,0 +1,22 @@
+const app = new Vue({
+    el:"#root",
+    data:{
+        emails:[]
+    },
+
+    created(){
+        for(i = 0; i < 10; i++){
+            axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
+                .then((response) => {
+                    // handle success
+                    this.emails.push(response.data.response);
+                })
+                .catch(function (error) {
+                    // handle error
+                    console.log(error);
+                })
+            }  
+            console.log(this.emails)
+        },
+    methods:{},
+})
