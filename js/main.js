fa2ae1ade4445336a8263d929704ea563a266b2e
AlexsandroD <adrdra88@gmail.com>
2021-12-09 19:38:14 +0100

    versione fetch async await

--- js/main.js
+++ js/main.js
@@ -1,22 +1,53 @@
-const app = new Vue({
-    el:"#root",
-    data:{
-        emails:[]
-    },
+// const app = new Vue({
+//     el:"#root",
+//     data:{
+//         emails:[]
+//     },
 
-    created(){
-        for(i = 0; i < 10; i++){
-            axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
-                .then((response) => {
-                    // handle success
-                    this.emails.push(response.data.response);
-                })
-                .catch(function (error) {
-                    // handle error
-                    console.log(error);
-                })
-            }  
-            console.log(this.emails)
-        },
-    methods:{},
-})
+//     created(){
+//         for(i = 0; i < 10; i++){
+//             axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
+//                 .then((res) => {
+//                     // handle success
+//                     this.emails.push(res.data.response);
+//                 })
+//                 .catch(function (error) {
+//                     // handle error
+//                     console.log(error);
+//                 })
+//             }  
+//             console.log(this.emails)
+//         },
+//     methods:{
+
+        
+
+//     },
+// })
+
+
+let mail = document.getElementById('emails');
+let newArr = [];
+
+for(let i = 0; i < 10; i++){
+    async function main (){
+        const res = await fetch('https://flynn.boolean.careers/exercises/api/random/mail');
+        const json = await res.json();
+        newArr.push(json.response);
+            displayElement(newArr)
+            console.log(newArr)        
+    }
+    main() 
+}
+
+
+function displayElement(element){
+    if(newArr.length == 10){
+        for(let k = 0; k < newArr.length; k++){
+            mail.innerHTML += `<li>${element[k]}</li>`
+        }
+    }
+    }
+
+
+
